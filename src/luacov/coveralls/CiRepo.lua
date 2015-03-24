35c4946f815813981b081898d0d0c12187737fa0
Alexey Melnichuk <mimir@newmail.ru>
2015-03-24 15:05:59 +0400

    Add. debug prints

--- src/luacov/coveralls/CiRepo.lua
+++ src/luacov/coveralls/CiRepo.lua
@@ -7,6 +7,7 @@ local function try_any_repo(repo_path)
   if repo then return repo, "git" end
 
   -- @todo warning message about failure
+  io.stderr:write("LUACOV: ", tostring(repo_path), ": Git error detect fail:", tostring(err), "\n")
 
   local function dummy() end
   return setmetatable({}, {__index = function() return dummy end}), "unknown"
